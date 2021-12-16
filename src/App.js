d45985dcd59805ad59689ff8f48e31fca84df468
Crawford Vining <crawpdx@gmail.com>
2021-12-15 17:30:17 -0800

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useImperativeHandle } from 'react'
 import './App.css';
+import axios from 'axios'
 
 function App() {
+  const [drinks, setDrinks] = useState([])
+  const [searchTerm, setSearchTerm] = useState('whiskey')
+
+ const handleSearch = (evt) => {
+      evt.preventDefault();
+      setSearchTerm(evt.target.value)
+  }
+
+  useEffect(() => {
+    const loadPost = async () => {
+      const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+      setDrinks(response.data.drinks)
+    }
+    loadPost()
+    console.log(drinks)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find a drink!</h1>
+        <input 
+          type="text"
+          value={searchTerm}
+          onChange={handleSearch}
+          placeholder="search"/>
+          <button>Search</button>
+      {
+       (drinks.map((drink) => 
+      <h4 key={drink.idDrink}>{drink.strDrink}</h4>
+      ))
+    }
     </div>
   );
 }
 
 export default App;
+
+
