52d8db77daf086453577ced40b62a2d06484cc59
Crawford Vining <crawpdx@gmail.com>
2021-12-16 07:41:30 -0800

    thacher fixed issue

--- src/App.js
+++ src/App.js
@@ -6,29 +6,49 @@ function App() {
   const [drinks, setDrinks] = useState([])
   const [searchTerm, setSearchTerm] = useState('whiskey')
 
- const handleSearch = (evt) => {
-      evt.preventDefault();
-      setSearchTerm(evt.target.value)
-  }
+//  const handleSearch = (evt) => {
+//       evt.preventDefault();
+//       setSearchTerm(evt.target.value)
+//   }
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const loadPost = async () => {
+  //     const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+  //     setDrinks(response.data.drinks)
+  //   }
+  //   loadPost()
+  //   // console.log(drinks)
+    
+  // }, [searchTerm])
+
+  const handleChange = (evt) => {
+    console.log(evt.target)
+    setSearchTerm(evt.target.value)
+}
+
+  const onSubmit = (evt) => {
+    evt.preventDefault();
+
     const loadPost = async () => {
       const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
       setDrinks(response.data.drinks)
     }
-    loadPost()
-    console.log(drinks)
-  }, [searchTerm])
+    loadPost()  
+    console.log(searchTerm)  
+  }
 
   return (
     <div className="App">
       <h1>Find a drink!</h1>
+      <form onSubmit={onSubmit}>
         <input 
           type="text"
           value={searchTerm}
-          onChange={handleSearch}
+          onChange={handleChange}
           placeholder="search"/>
-          <button>Search</button>
+        <button type="submit">Search</button>
+      </form>
+        
       {
        (drinks.map((drink) => 
       <h4 key={drink.idDrink}>{drink.strDrink}</h4>
